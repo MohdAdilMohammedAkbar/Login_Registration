765f83a1589fc09a23e3641cecc70ac8ebf44233
mohdadil <mdadiladakbar@gmail.com>
2024-02-29 00:53:18 +0530

    THis is the second commmit

--- app/src/main/java/com/example/kotlinlogin_2/Login_Registration.kt
+++ app/src/main/java/com/example/kotlinlogin_2/Login_Registration.kt
@@ -7,5 +7,13 @@ class Login_Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_registration)
+
+        if (savedInstanceState == null) {
+            // Create and set the default fragment
+            val defaultFragment = LoginFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_login_reg, defaultFragment)
+                .commit()
+        }
     }
 }
